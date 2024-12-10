624123cdb8238155778bab5371fb86e81ab14d9d
haozhang <haozhan@microsoft.com>
2024-12-10 16:50:37 +0800

    revert test change

--- spring-petclinic-customers-service/src/main/resources/db/mysql/data.sql
+++ spring-petclinic-customers-service/src/main/resources/db/mysql/data.sql
@@ -15,7 +15,6 @@ INSERT IGNORE INTO owners VALUES (7, 'Jeff', 'Black', '1450 Oak Blvd.', 'Monona'
 INSERT IGNORE INTO owners VALUES (8, 'Maria', 'Escobito', '345 Maple St.', 'Madison', '6085557683');
 INSERT IGNORE INTO owners VALUES (9, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Madison', '6085559435');
 INSERT IGNORE INTO owners VALUES (10, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', '6085555487');
-INSERT IGNORE INTO owners VALUES (11, 'Hao', 'Zhang', 'Zixing Road 999, Minhang', 'Shanghai', '1234567890');
 
 INSERT IGNORE INTO pets VALUES (1, 'Leo', '2000-09-07', 1, 1);
 INSERT IGNORE INTO pets VALUES (2, 'Basil', '2002-08-06', 6, 2);
